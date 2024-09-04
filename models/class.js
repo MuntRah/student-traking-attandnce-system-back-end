f1691c0cc6dc4844af9caa696df823491739f6d4
MuntRah <me.alrahim.02@gmail.com>
2024-09-04 15:36:17 +0300

    i make some changes on server js and i add the ctrls and i make some changes on the controller and the models

--- models/class.js
+++ models/class.js
@@ -1,5 +1,6 @@
 const classSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  teacherName:{type:String , required: true},
   subject: { type: String, required: true },
   teacher: {
     type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'teacher', 'student'], required: true },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
 });
