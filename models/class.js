61a81eec7efa3042836d8197c764653738f03a11
MuntRah <me.alrahim.02@gmail.com>
2024-09-04 12:38:11 +0300

    -

--- models/class.js
+++ models/class.js
@@ -1,9 +1,13 @@
 const classSchema = new mongoose.Schema({
   name: { type: String, required: true },
   subject: { type: String, required: true },
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: true },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Teacher",
+    required: true,
+  },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
 });
 
-const Class = mongoose.model('Class', classSchema);
+const Class = mongoose.model("Class", classSchema);
 module.exports = Class;
--- models/user.js
+++ models/user.js
@@ -1,28 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'teacher', 'student'], required: true },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
-=======
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  hashedPassword: {
-    type: String,
-    required: true,
-  },
-  assignedClass: { type: mongoose.Schema.Types.ObjectId, ref: "Class" },
-  isTeacher: { type: Boolean },
-
-  isAdmint: {type: Boolean , default: false}
->>>>>>> 1079d5d13457de7b4a8c99cf43ef78cdc5668ad6
 });
 
 const User = mongoose.model('User', userSchema);
